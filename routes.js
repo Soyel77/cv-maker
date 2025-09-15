f2f8ba7f360b22480e2c391c02f876a89299e1e3
Soyel77 <wbrebeka@gmail.com>
2025-09-15 11:50:22 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("./user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
